ab050c95e4ab311310e28901444c116fd49fd47e
Dafydd James <dafydd@cantab.net>
2012-04-05 23:30:01 +0100

    Swap to use pyral.

--- rallycli.py
+++ rallycli.py
@@ -5,10 +5,12 @@ Rally client.
 
 import sys
 import os
-from pyrally import RallyAPIClient, Story, Defect
+import argparse
+import simplejson as json
+from pyral import Rally, rallySettings
 from config import RallyCliConfig
 
-import argparse
+options = [opt for opt in sys.argv[1:] if opt.startswith('--')]
 
 def parseArgs():
 	''' Retrieve arguments from the command line. '''
@@ -21,27 +23,38 @@ def parseArgs():
 	args = parser.parse_args()
 	return args
 
-def retrieveItems(itemIds):
-	''' Use rallypy to retrieve items from Rally. '''
+def retrieveItems(rally, itemIds):
+	''' Use pyral to retrieve items from Rally. '''
 	items = {}
 	for itemId in itemIds:
-		items[itemId] = rac.get_entity_by_formatted_id(itemId)
+		query_criteria = 'formattedID = "%s"' % itemId
+		# print query_criteria
+		response = rally.get('HierarchicalRequirement', fetch=True, query=query_criteria)
+		# print json.dumps(response.content)
+		if response.errors:
+		    sys.stdout.write("\n".join(response.errors))
+		    sys.exit(1)
+		for item in response:  # there should only be one qualifying item
+			# items[itemId] = item
+		    print "%s: %s (%s)" % (item.FormattedID, item.Name, item.Project.Name)
 	return items
 
 if __name__ == "__main__":
 	config = RallyCliConfig()
 	args = parseArgs()
-	itemIds = args.workItems
-	# itemIds = readitemIdsFromCommandLine()
+
 	print "Connecting to Rally as %s..." % (config.username)
-	rac = RallyAPIClient(config.username, config.password, config.rallyUrl)
+	rally = Rally(config.rallyUrl, config.username, config.password, workspace="Betfair", project="Betfair")
+	rally.enableLogging('rallycli.log')
+
+	itemIds = args.workItems
 	print "Retrieving items %s..." % (itemIds)
-	items = retrieveItems(itemIds)
-	for itemId, item in items.iteritems():
-		if item == None:
-			print "ERROR: Could not retrieve item %s from Rally" % itemId
-		else:
-			if args.format == "confluence" :
-				print "# [%s: %s|%s]" % (itemId, item.title, item.rally_url)
-			else:
-				print "%s: %s (%s)" % (itemId, item.title, item.rally_url)
+	items = retrieveItems(rally, itemIds)
+	# for itemId, item in items.iteritems():
+	# 	if item == None:
+	# 		print "ERROR: Could not retrieve item %s from Rally" % itemId
+	# 	else:
+	# 		if args.format == "confluence" :
+	# 			print "# [%s: %s|%s]" % (itemId, item.title, item.rally_url)
+	# 		else:
+	# 			print "%s: %s (%s)" % (itemId, item.title, item.rally_url)
